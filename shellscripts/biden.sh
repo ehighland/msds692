0becda92d5672c0b008c55bb9a22da2cf5a5f315
Emma Highland <ehighland@luc.edu>
2019-10-12 14:31:28 -0500

    Adding the rest of the files including text, scripts, and jupyter notebook

---
+++ shellscripts/biden.sh
@@ -0,0 +1,26 @@
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-08-08 --until 2019-08-09 --maxtweets 100
+mv output_got.csv biden0808.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-08-15 --until 2019-08-16 --maxtweets 100
+mv output_got.csv biden0815.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-08-27 --until 2019-08-28 --maxtweets 100
+mv output_got.csv biden0827.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-07 --until 2019-09-08 --maxtweets 100
+mv output_got.csv biden0907.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-11 --until 2019-09-12 --maxtweets 100
+mv output_got.csv biden0911.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-12 --until 2019-09-13 --maxtweets 100
+mv output_got.csv biden0912.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-17 --until 2019-09-18 --maxtweets 100
+mv output_got.csv biden0917.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-21 --until 2019-09-22 --maxtweets 100
+mv output_got.csv biden0921.csv
+
+GetOldTweets3 --querysearch "'Joe Biden' OR #JoeBiden" --since 2019-09-24 --until 2019-09-25 --maxtweets 100
+mv output_got.csv biden0924.csv
---
+++ shellscripts/warren.sh
@@ -0,0 +1,26 @@
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-08-08 --until 2019-08-09 --maxtweets 100
+mv output_got.csv warren0808.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-08-15 --until 2019-08-16 --maxtweets 100
+mv output_got.csv warren0815.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-08-27 --until 2019-08-28 --maxtweets 100
+mv output_got.csv warren0827.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-07 --until 2019-09-08 --maxtweets 100
+mv output_got.csv warren0907.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-11 --until 2019-09-12 --maxtweets 100
+mv output_got.csv warren0911.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-12 --until 2019-09-13 --maxtweets 100
+mv output_got.csv warren0912.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-17 --until 2019-09-18 --maxtweets 100
+mv output_got.csv warren0917.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-21 --until 2019-09-22 --maxtweets 100
+mv output_got.csv warren0921.csv
+
+GetOldTweets3 --querysearch "'Elizabeth Warren' OR #ElizabethWarren" --since 2019-09-24 --until 2019-09-25 --maxtweets 100
+mv output_got.csv warren0924.csv
